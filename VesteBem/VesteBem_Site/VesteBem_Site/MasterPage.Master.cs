a4969871f15c2e3d90b2400f63a69d9cf0884a22
KromKiller <36567169+KromKiller@users.noreply.github.com>
2021-05-31 19:48:50 +0100

    login a funcionar corretamente

--- VesteBem/VesteBem_Site/VesteBem_Site/MasterPage.Master.cs
+++ VesteBem/VesteBem_Site/VesteBem_Site/MasterPage.Master.cs
@@ -10,66 +10,94 @@ using VesteBem_Site;
 
 namespace VesteBem
 {
-	public partial class MasterPage : System.Web.UI.MasterPage
+    public partial class MasterPage : System.Web.UI.MasterPage
 
-	{
-		SqlConnection liga = new SqlConnection(@"Server=tcp:srv-epbjc.database.windows.net,1433;Initial Catalog=bd;Persist Security Info=False;User ID=epbjc;Password=Teste123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-		SqlCommand comando = new SqlCommand();
-		SqlDataReader dr;
+    {
+        SqlConnection liga = new SqlConnection(@"Server=tcp:srv-epbjc.database.windows.net,1433;Initial Catalog=bd;Persist Security Info=False;User ID=epbjc;Password=Teste123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
-		protected void Log_Click(object sender, EventArgs e)
-		{
-			try
-			{
-				liga.Open();
-				comando.Connection = liga;
-				comando.CommandText = "Select Passw from tbl_login where Usern like '" + uname.Value + "'";
-				dr = comando.ExecuteReader();
-				
-				if (dr.Read())
-				{
-					string pass = dr[0].ToString();
+        SqlCommand comando = new SqlCommand();
 
-					
+        SqlDataReader dr;
 
-					if (psw.Value == EncryptADeDecrypt.DecryptRSA(dr["Passw"].ToString()))
-					{
-						Session["Username"] = uname.Value;
-						Response.Write("<script>alert('Sessão efetuada com sucesso')</script>");
-						Response.Redirect("~/logincerto.aspx");
-					}
-					else
+        bool entrou;
+
+
+
+        protected void Log_Click(object sender, EventArgs e)
+
+        {
+
+            try
+
+            {
+
+                liga.Open();
+
+                comando.Connection = liga;
+
+                comando.CommandText = "Select Passw from tbl_login where Usern like '" + uname.Value + "'";
+
+                dr = comando.ExecuteReader();
+
+
+
+                if (dr.Read())
+
+                {
+
+                    string pass = dr[0].ToString();
+
+
+
+
+
+                    if (psw.Value == EncryptADeDecrypt.DecryptRSA(dr["Passw"].ToString()))
+
                     {
-						Response.Write("<script>alert('Pass ou Username/Email Inválidos!!!')</script>");
-						Response.Redirect("~/Teste.aspx");
-					}
-					
 
-				}
-				
+                        Session["Username"] = uname.Value;
 
-			}
-			catch {
+                        Response.Write("<script>alert('Sessão efetuada com sucesso')</script>");
 
-				Response.Write("<script>alert('login mal sucedido')</script>");
-			
-			}
-		}
+                        entrou = true;
+
+                    }
+
+                    else
+
+                    {
+
+                        Response.Write("<script>alert('Pass ou Username/Email Inválidos!!!')</script>");
+
+                        entrou = false;
+
+                    }
 
 
-		protected void btnSearch_Click(object sender, EventArgs e)
-		{
-			txtSearchMaster.Visible = true;
-			if (txtSearchMaster.Text != "")
-			{
-				var searchText = Server.UrlEncode(txtSearchMaster.Text); // URL encode in case of special characters
-				Response.Redirect("~/MasterPage.aspx?srch=" + searchText);
-			}
-		}
 
-		protected void Page_Load(object sender, EventArgs e)
-		{
-			txtSearchMaster.Visible = false;
-		}
-	}
+
+
+                }
+
+
+
+
+
+            }
+
+            catch
+            {
+
+
+
+                Response.Write("<script>alert('login mal sucedido')</script>");
+
+
+
+            }
+
+               
+            
+        }
+    }
 }
